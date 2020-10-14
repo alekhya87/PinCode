46c68f7e0e3bfb1bf6d3e6a804b5ad07c19f5783
alekhya87 <davulurialekhya87909.com>
2020-10-14 11:20:49 +0530

    PINCodeUC-4 is done

--- pincode.sh
+++ pincode.sh
@@ -1,2 +1,11 @@
 #!/bin/bash/ -x
 echo "the postal index number"
+echo "enter the Postal Index Number"
+read pin
+pinPattern="^[0-9]{6}$|^[0-9]{3}[[:space:]][0-9]{3}$"
+   if [[ $pin =~ $pinPattern ]]
+   then
+      echo "valid pincode" 
+   else
+      echo "invalid pincode"
+   fi
